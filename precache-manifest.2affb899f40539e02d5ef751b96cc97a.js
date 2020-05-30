8b9cb49a24634406c5aef318cc310e8417981b71
suryan535 <suryan535@gmail.com>
2020-05-30 19:26:54 +0530

    Updates

---
+++ precache-manifest.2affb899f40539e02d5ef751b96cc97a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1e29d5223032494a536c80518a3b627",
+    "url": "/TA_App/index.html"
+  },
+  {
+    "revision": "4e79761b9b66a67ae422",
+    "url": "/TA_App/static/css/main.7dd09325.chunk.css"
+  },
+  {
+    "revision": "a1f1019c536ce6880353",
+    "url": "/TA_App/static/js/2.bfd56e6a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TA_App/static/js/2.bfd56e6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e79761b9b66a67ae422",
+    "url": "/TA_App/static/js/main.093b28da.chunk.js"
+  },
+  {
+    "revision": "35657d7d9fc3dc4d5a78",
+    "url": "/TA_App/static/js/runtime-main.ad756469.js"
+  },
+  {
+    "revision": "926a2059ca8d487b9c22c475a5309524",
+    "url": "/TA_App/static/media/Chemistry.926a2059.svg"
+  },
+  {
+    "revision": "6a7836d791ba936ba209904506042217",
+    "url": "/TA_App/static/media/Electronics.6a7836d7.svg"
+  },
+  {
+    "revision": "7a21fd5110fb7080ff6321e723add915",
+    "url": "/TA_App/static/media/English.7a21fd51.svg"
+  },
+  {
+    "revision": "d56951a6ac029e2700384de45dd343cc",
+    "url": "/TA_App/static/media/HomeImage.d56951a6.jpg"
+  },
+  {
+    "revision": "95d3065837388b1df643ab651640510f",
+    "url": "/TA_App/static/media/Maths.95d30658.svg"
+  },
+  {
+    "revision": "a2e9f12183572cd18f5da3508e565a37",
+    "url": "/TA_App/static/media/Mechanics.a2e9f121.svg"
+  },
+  {
+    "revision": "af5bd4af0726c3d2cb3459dde40d7a6a",
+    "url": "/TA_App/static/media/Physics.af5bd4af.svg"
+  },
+  {
+    "revision": "5f7016542de7391d40b70c260516eb88",
+    "url": "/TA_App/static/media/Student.5f701654.svg"
+  }
+]);
